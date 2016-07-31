45e536ed0b62c1bf6712e98ae5e70a0c63a3e520
twoplayergames <bellcairemoto@gmail.com>
2016-07-31 19:04:43 +0200

    Index- movile compatibility

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	var zoom 
+	function zoom(x){
+		if (window.innerWidth < window.innerHeight) {
+			zoom = (window.innerWidth * x / 1280) + '%';
+		} else {
+			zoom = (window.innerHeight * x / 683) + '%';
+		}
+	$('body').css('zoom', zoom);
+	} 
+	
+	if (window.innerWidth > 800) {
+		$('#contenidor').css('width', '750px');
+		zoom(100);
+	} else {
+		$('#contenidor').css('width', '250px');
+		$('.descript').css('zoom', (window.innerWidth * 100 / 720) + '%');
+		$('button') .css('zoom', '200%');
+		zoom(200);
+	}
+	
+	
+	$('#memory').click(function(){
+		$('#dmemory').fadeIn();
+	});
+	
+	$('#tictactoe').click(function(){
+		$('#dtictactoe').fadeIn();
+	});
+	
+	$('#fingers').click(function(){
+		$('#dfingers').fadeIn();
+	});
+	
+	$('#connect4').click(function(){
+		$('#dconnect4').fadeIn();
+	});
+	
+	$('.close').click(function(){
+		$('#dfingers').fadeOut();
+		$('#dtictactoe').fadeOut();
+		$('#dmemory').fadeOut();
+		$('#dconnect4').fadeOut();
+	});
+});
